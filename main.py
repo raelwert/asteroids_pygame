eb568bc4a9cb088303a7775df6a2fcde68099383
raelwert <raelwert@gmail.com>
2025-10-20 18:31:57 -0400

    drawing player ship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -10,11 +11,14 @@ def main():
 
     wall_clock = pygame.time.Clock()
     dtForFrame = 0
+
+    player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dtForFrame = wall_clock.tick(60)
 
